a804bf3b75c5a948554d6be0683343270a4d149e
Mykhailo Ziatin <mikhail@datarobot.com>
2015-07-09 11:21:31 +0300

    Validate HDFS file existence.

--- spark-job-rest-api/src/main/scala/utils/ContextUtils.scala
+++ spark-job-rest-api/src/main/scala/utils/ContextUtils.scala
@@ -9,6 +9,15 @@ import scala.collection.JavaConverters._
 object ContextUtils {
   private val log = LoggerFactory.getLogger(getClass)
 
+  /**
+   * Creates Spark config from application config.
+   *  - Copies everything that starts from `spark.` but not from `spark.job.rest.`.
+   *  - Set jars from `spark.job.rest.context.jars`.
+   *  - Set application name to context name.
+   * @param config application config
+   * @param contextName context name
+   * @return
+   */
   def configToSparkConf(config: Config, contextName: String): SparkConf = {
     val jars = config.getStringList("spark.job.rest.context.jars").asScala.toSeq
     log.info(s"Jars in config for $contextName: $jars")
--- spark-job-rest/src/main/scala/utils/HdfsUtils.scala
+++ spark-job-rest/src/main/scala/utils/HdfsUtils.scala
@@ -1,26 +1,36 @@
 package utils
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{Path, FileSystem}
+import org.apache.hadoop.fs.{FileSystem, Path}
 
 /**
- * Created by raduchilom on 22/03/15.
+ * HDFS utility functions.
  */
 object HdfsUtils {
-
-  def copyJarFromHdfs(hdfsPath: String, outputFolder: String) = {
-
-    //    if(!config.hasPath("hdfs.namenode")){
-    //      println("ERROR: HDFS NameNode is not set in application.conf!")
-    //      throw new Exception("HDFS NameNode is not set in application.conf!")
-    //    }
-
+  /**
+   * Copies file from HDFS to local file system.
+   * @param hdfsPath HDFS path
+   * @param outputFolder local path
+   * @throws java.io.IOException if copy operation failed
+   */
+  def copyJarFromHdfs(hdfsPath: String, outputFolder: String): Unit = {
     val conf = new Configuration()
-    //    conf.set("fs.defaultFS", getValueFromConfig(config, "hdfs.namenode", ""))
     conf.set("fs.defaultFS", hdfsPath)
     val hdfsFileSystem = FileSystem.get(conf)
-
     hdfsFileSystem.copyToLocalFile(new Path(hdfsPath), new Path(outputFolder))
+  }
+
+  /**
+   * Checks whether file exists.
+   * @param hdfsPath HDFS path
+   * @return
+   * @throws java.io.IOException on HDFS failure
+   */
+  def hdfsFileExists(hdfsPath: String): Boolean = {
+    val conf = new Configuration()
+    conf.set("fs.defaultFS", hdfsPath)
+    val hdfsFileSystem = FileSystem.get(conf)
+    hdfsFileSystem.exists(new Path(hdfsPath))
   }
 
 }
